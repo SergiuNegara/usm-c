11aad9be155cca9fe9d01199d4c123630a7fec75
Sergiu Negara <negara.sergiu@gmail.com>
2017-12-14 02:37:01 +0200

    Add lab4: output formatting Implements: printf

---
+++ lab4/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("\n1234567890123456789012\n");
+
+    /*  1 */ printf("%7s%8s\n", "NEGARA", "SERGIU");
+    /*  2 */ printf("%8.1f\n", 12.5);
+    /*  3 */ printf("%19s\n", "SOLIDARITATE");
+    /*  4 */ printf("%8s\n", "MIRACOL");
+    /*  5 */ printf("%12.1f\n", 22.3);
+    /*  6 */ printf("%5.1f\n", 14.3);
+    /*  7 */ printf("%13.1f%9.2f\n", 15.3, 240.15);
+    /*  8 */ printf("%0*d%2d%15s\n", 3, 0, 22, "***");
+    /*  9 */ printf("%8.1f%9.1f\n", 0.5, 16.8);
+    /* 10 */ printf("%s%17.1f\n", "***", 22.3);
+    /* 11 */ printf("%5s\n", "**");
+    /* 12 */ printf("%6s%9.1f\n", "*", 14.5);
+    /* 13 */ printf("%7s\n", "*");
+    /* 14 */ printf("%8s%10.1f\n", "*", 16.8);
+    /* 15 */ printf("%7.1f\n", 15.8);
+    /* 16 */ printf("%11d\n", 420);
+    /* 17 */ printf("%17.1f\n", 16.8);
+    /* 18 */ printf("%s%13d\n", "MIRACOL", 430);
+    /* 19 */ printf("%13s\n", "ANA");
+    /* 20 */ printf("%10s%6d\n", "VICTORIA", 425);
+
+    printf("1234567890123456789012\n");
+
+    return 0;
+}
